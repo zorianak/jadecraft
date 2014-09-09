057e9b9d153040414f7d1dad2862126005c584fd
Kim Holmes <yes@kimiscool.me>
2014-09-08 21:39:44 -0700

    Now imports Caligraphy!

--- public/javascripts/char-import.js
+++ public/javascripts/char-import.js
@@ -1,7 +1,21 @@
 function importChar(region, char, realm) {
-    var url = region + 'battle.net' +'/api/wow/character' + realm + '/' + char;
-    var statsUrl = url + '?fileds=stats';
+    // http://us.battle.net/api/wow/character/Windrunner/Caligraphy?fields=stats
+    var url = 'http://' + region + '.battle.net' +'/api/wow/character/' + realm + '/' + char;
+    var statsUrl = url + '?fields=stats';
     
     // So we should be sent data with all the char dataz heh
+//    $.getJSON(statsUrl , function( data ) {
+//        var items = [];
+//        console.log(items);
+//    });
     
+    $.ajax({
+        type: "GET",
+        url: statsUrl,
+        dataType: "jsonp",
+        success: function(data){
+            var name = data["name"];
+            console.log(name);   
+        }
+    });
 }
---
+++ public/javascripts/global.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    importChar('us', 'Caligraphy', 'Windrunner');
+});
