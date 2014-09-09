765071d47a02e314c0249fe3b68cb30e63c024cc
Kim Holmes <kim.holmes@ask.com>
2014-09-09 12:48:24 -0700

    Fixed callback, creating charObj

--- public/javascripts/char-import.js
+++ public/javascripts/char-import.js
@@ -1,7 +1,9 @@
 function importChar(region, char, realm) {
     // http://us.battle.net/api/wow/character/Windrunner/Caligraphy?fields=stats
-    var url = 'http://' + region + '.battle.net' +'/api/wow/character/' + realm + '/' + char;
-    var statsUrl = url + '?fields=stats&locale=en_US&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
+    var url = 'https://' + region + '.battle.net' +'/api/wow/character/' + realm + '/' + char;
+    var statsUrl = url + '?fields=stats&locale=en_US&jsonp=callback&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
+
+    var charObj = [];
     
     $.ajax({
         url: statsUrl,
@@ -9,7 +11,33 @@ function importChar(region, char, realm) {
         jsonpCallback: 'callback',
         type: 'GET',
         success: function (data) {
-            console.log(data);
+            // so now we want to assign these to charObj
+            charObj["name"] = data["name"];
+            charObj["realm"] = data["realm"];
+            charObj["race"] = data["race"];
+
+            // now the real stats!
+            charObj["agi"] = data["stats"]["agi"];
+            charObj["str"] = data["stats"]["str"];
+            charObj["ap"] = data["stats"]["attackPower"];
+            charObj["crit"] = data["stats"]["critRating"];
+            charObj["mastery"] = data["stats"]["masteryRating"];
+            charObj["haste"] = data["stats"]["hasteRating"];
+            // 6.0 stats?
+            //charObj["versatility"] = data["stats"]["versatilityRating"];
+            charObj["Mdps"] = data["stats"]["mainHandDmgMax"];
+            charObj["mdps"] = data["stats"]["mainHandDmgMin"];
+            charObj["mhs"] = data["stats"]["mainHandSpeed"]
+            charObj["ohdps"] = data["stats"]["offHandDmgMin"];
+            charObj["Ohdps"] = data["stats"]["offHandDmgMax"];
+            charObj["ohs"] = data["stats"]["offHandSpeed"];
+
+
+            console.log(charObj["agi"]);
         }
     });
+
+    // Charsheet will have everything nicely packaged into a new object
+    // from the API's return.
+    return charObj;
 }
