8e0f7d9d0b28efee2aa73f6ae61dc46dfd61b510
Kim Holmes <yes@kimiscool.me>
2014-09-09 21:07:34 -0700

    blackout kick damage and dot damage

--- public/javascripts/spells/blackoutKick.js
+++ public/javascripts/spells/blackoutKick.js
@@ -1,1 +1,15 @@
 var blackoutkick = new spell(physical, 5.375, mhdps, avgWDps, ap, ifTp, stance, minWdps, maxWdps);
+
+// for the dot
+
+var bokDot = .3;
+
+var bokDotDmg = function(){
+    if(offhand) {
+        var damage = blackoutkick.dwDamage() * bokDot;
+    } else {
+        var damage = blackoutkick.twoHandDamage() * bokDot;
+    }
+    
+    return damage;
+}
