16354dca331f24093b6fb803b9975e84fe867b9f
Kim Holmes <yes@kimiscool.me>
2014-09-09 19:17:18 -0700

    2h vs dw dps

--- public/javascripts/spells/spell-base.js
+++ public/javascripts/spells/spell-base.js
@@ -1,10 +1,43 @@
-function spellBase(school, coefficient, mhdps, Mhdps, ap){
+/***************************************************************
+
+This is the base spell object. This is what all other spells 
+inherit from.
+
+HUGE shoutout to Gahddo for helping with spell formula and
+other explanations for this shiznap.
+
+***************************************************************/
+function spellBase(school, coefficient, mhdps, avgWDps, ap, ifTp, stance, minWdps, maxWdps){
     this.school = school;
     this.estimate = estimate;
     this.coefficient = coefficient;
+    this.avgWdps = avgWDps
+    this.stance = stance;
+    this.minWdps = minWdps
+    
+    if (this.school = physical) {
+            if(ifTp){
+                var armor = 0.72685539403;
+            } else {
+                var armor = 0.6506849315;
+            }
+            
+            // from:
+            // https://docs.google.com/spreadsheets/d/1Nz81LRgnPmT1cAmShIX8emljDKFEOtDXw9IrGO9kJ8c/edit#gid=1356822350
+        }
     
     this.twoHandDamage = function() {
-        var minDmg = coefficient * mhdps + (ap/3.5);
-        var maxDmg = coefficient * Mdps + (ap/3.5);
+        // credit to Gahddo
+        var damage = this.coefficient * this.stance * ( (( avgWdps ) / 2 ) + ( ap / 3.5 ) );
+        
+        return damage;
+    }
+    
+    this.dwDamage = function(){
+        // more Gahddo creddit
+        var dwMod = 0.898882275;
+        var damage = this.coefficient * 1.1 * ( ((( this.minWdps * 0.5 ) + ( this.maxWdps * 0.5 ) ) / 2 ) *  dwMod + ( ap / 3.5 ) );
+        
+        return damage;
     }
 }
