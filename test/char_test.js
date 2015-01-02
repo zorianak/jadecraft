e7a860939a8c80285ab1b591f355a92385d71f9f
Kim Holmes <yes@kimiscool.me>
2015-01-01 17:36:38 -0800

    Dont need test functions now

--- test/char_test.js
+++ test/char_test.js
@@ -5,22 +5,8 @@ var chai = require('chai'),
     should = require('chai').should(),
     expect = require('chai').expect,
     charscript = require('../public/javascripts/char'),
-    escape = charscript.escape,
-    unescape = charscript.unescape,
     Char = charscript.Char;
 
-//describe("Char", function() {
-//    describe("constructor", function() {
-//        it("should have a default name", function() {
-//            var result = new Char();
-//            expect(result.name).to.equal("Anon monk");
-//        });
-//        it("should set name if provided in json", function(){
-//           var result = new Char({"name": "Monk"});
-//            expect(result.name).to.equal("Monk");
-//        });
-//    });
-//});
 
 describe('#char', function() {
     it('should have a default name', function() {
@@ -31,26 +17,4 @@ describe('#char', function() {
         var result = new Char({"name": "Monk"});
         expect(result.name).to.equal('Monk');
     });
-});
-
-describe('#escape', function() {
-  it('converts & into &amp;', function() {
-    escape('&').should.equal('&amp;');
-  });
-
-  it('converts " into &quot;', function() {
-    escape('"').should.equal('&quot;');
-  });
-
-  it('converts \' into &#39;', function() {
-    escape('\'').should.equal('&#39;');
-  });
-
-  it('converts < into &lt;', function() {
-    escape('<').should.equal('&lt;');
-  });
-
-  it('converts > into &gt;', function() {
-    escape('>').should.equal('&gt;');
-  });
 });
