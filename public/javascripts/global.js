532ea1451eb5526bc1b19f141eabbad2c5264c27
Kim Holmes <yes@kimiscool.me>
2015-01-06 19:44:44 -0800

    We can now pull from template for important javascript things

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,12 +1,25 @@
 
 $(document).ready(function(){
 
-    importChar('US', 'Caligraphy', 'Windrunner', function(data) {
-        var theChar = data;
-
-    });
-
     // test for TP to be active - yes globalized
     ifTp = $('#ifTp').val();
 
+    // get all the character stats from the table.
+    // This basically allows us to automagically
+    // allow the model to happen; sure,
+    // client-side is bad, but we'll forgive it for
+    // now.
+    var charStats = {};
+    var theTable = $('#char-stat-table tr');
+    $.each( theTable, function( key, value ) {
+        var attribute = $($(theTable)[key]).find('td.attrName').html();
+        var attrRating = $($(theTable)[key]).find('td.attrStat').html();
+        charStats[attribute] = attrRating;
+    });
+
+    // for now we're also going to add some of the
+    // weapon information into the tables, so that
+    // we can more easily get the wDPS
+
+    console.log(charStats);
 });
