b48ec80ffe8d62a0c6d41049ad00bb14febed577
jg <vulpineblazeyt@gmail.com>
2015-01-14 21:54:41 -0800

    URL param successfully tested

--- routes/chars.js
+++ routes/chars.js
@@ -4,17 +4,55 @@ var express = require('express'),
     importChar = npmarmory.importChar,
     Char = npmarmory.Char;
 
-// let's see if we can break node here.  ._. prelim logic
-// to import my monk
+//need theChar obj early
+var theChar = "";
 
-importChar('US', 'Caligraphy', 'Windrunner', function(data) {
+// defaults
+var jadeRegion = "US";
+var jadeName = "Caligraphy";
+var jadeRealm = "Windrunner";
 
-    // now send them to the page?
-    /* GET chars listing. */
-    router.get('/', function(req, res) {
-      var theChar = new Char(data);
-      res.render('char', { "theChar": theChar });
-    });
+
+
+router.get('/', function(req, res) {
+  //make some tmps to catch param data
+  var tmpRealm, tmpRegion,tmpName;
+
+  //populate param
+  tmpRegion = ""+req.param("region");
+  tmpName = ""+req.param("name");
+  tmpRealm = ""+req.param("realm");
+
+  // if param exist overwrite defaults
+  if(tmpRegion && tmpRegion != "" && tmpRegion != "undefined"){
+    jadeRegion = tmpRegion;
+  };
+  if(tmpName && tmpName != "" && tmpRegion != "undefined"){
+    jadeName = tmpName;
+  };
+  if(tmpRealm && tmpRealm != "" && tmpRegion != "undefined"){
+    jadeRealm = tmpRealm;
+  };
+
+  //call to npmarmory
+  importChar(jadeRegion,jadeName,jadeRealm, function(data) {
+    //get real char data
+    theChar = new Char(data);
+
+    //add params for output onto template
+    theChar.jadeRegion = jadeRegion;
+    theChar.jadeName = jadeName;
+    theChar.jadeRealm = jadeRealm;
+    
+    // ps, never got console.log to work..  not sure what i did wrong
+    // console.log("test"); never appeared anywhere
+    // not the browser broser conslle nor the server itself
+    // even though "night elf" from npmarmory does
+
+  });
+
+  //send to template...  i think
+  res.render('char', { "theChar": theChar});
 });
 
 module.exports = router;
