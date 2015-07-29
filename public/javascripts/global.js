7292e515f158c08c6473b545a8fc6c2df3543249
Kim Holmes <yes@kimiscool.me>
2015-07-28 19:43:37 -0700

    Appear to have never ever added Multistrike

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -9,15 +9,6 @@ $(document).ready(function(){
     // weapon information into the tables, so that
     // we can more easily get the wDPS
 
-    var fof = new Jadec.spell('physical', 7.755, charStats, ifTp, 1.1, 25);
-
-    var fofDmg = function() {
-        var ticks = 5;
-        var damage = fof.damage() * ticks;
-
-        return damage;
-    }
-
     console.log((charStats["Haste"]/1000) + 10.00);
     console.log(charStats);
 });
